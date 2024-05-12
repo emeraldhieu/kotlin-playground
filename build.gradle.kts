1e419998cb05e28cb9372f669736aa93ea437751
Hieu <emerald.hieu@gmail.com>
2024-05-12 17:05:28 +0700

    Add Jupiter, testLogger, and format code

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,43 +1,81 @@
+import com.adarshr.gradle.testlogger.theme.ThemeType
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.2.1"
-	id("io.spring.dependency-management") version "1.1.4"
-	kotlin("jvm") version "1.9.21"
-	kotlin("plugin.spring") version "1.9.21"
+    id("org.springframework.boot") version "3.2.1"
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "1.9.21"
+    kotlin("plugin.spring") version "1.9.21"
+
+    // Test logging framework
+    id("com.adarshr.test-logger") version "4.0.0"
 }
 
 group = "com.emeraldhieu"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 val jacksonVersion = "2.16.1"
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework:spring-web")
 
-	implementation("org.modelmapper:modelmapper:3.2.0")
-	implementation("com.fasterxml.jackson.core:jackson-databind:${jacksonVersion}")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:${jacksonVersion}")
+    implementation("org.modelmapper:modelmapper:3.2.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:${jacksonVersion}")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:${jacksonVersion}")
 
+    /**
+     * Compile and execute Jupiter tests
+     * See https://docs.gradle.org/current/userguide/java_testing.html#compiling_and_executing_junit_jupiter_tests
+     */
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+/**
+ * Enables JUnit Platform support
+ * See https://docs.gradle.org/current/userguide/java_testing.html#using_junit5
+ */
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "21"
-	}
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "21"
+    }
 }
 
-tasks.withType<Test> {
-	useJUnitPlatform()
+/**
+ * Test logger configuration
+ * See https://github.com/radarsh/gradle-test-logger-plugin?tab=readme-ov-file#configuration
+ */
+testlogger {
+    theme = ThemeType.STANDARD
+    showExceptions = true
+    showStackTraces = true
+    showFullStackTraces = false
+    showCauses = true
+    slowThreshold = 2000
+    showSummary = true
+    showSimpleNames = false
+    showPassed = true
+    showSkipped = true
+    showFailed = true
+    showOnlySlow = false
+    showStandardStreams = false
+    showPassedStandardStreams = true
+    showSkippedStandardStreams = true
+    showFailedStandardStreams = true
+    logLevel = LogLevel.LIFECYCLE
 }
